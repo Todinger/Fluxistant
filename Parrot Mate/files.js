4e41a3951886adec0211dc49a64068084a2e394c
Tomer Godinger <Todinger@Gmail.com>
2020-09-02 01:45:39 +0300

    Initial entry for all, Parrot Mate effect works! =D

---
+++ Parrot Mate/files.js
@@ -0,0 +1,75 @@
+
+const IMAGE_LOCATIONS = {
+	regular:	'./Images/Parrot-Regular.png',
+	mouth:		'./Images/Parrot-Mouth.png',
+	look:		'./Images/Parrot-Look.png',
+	wing:		'./Images/Parrot-Wing.png',
+};
+
+const SOUND_LOCATIONS = {
+	hey:		'./Sounds/Hey! Look! Listen!.mp3',
+	laughter:	'./Sounds/Laughter.mp3',
+	behind:		'./Sounds/Look Behind You.mp3',
+	attention:	'./Sounds/Pay Me Attention.mp3',
+	cracker:	'./Sounds/Polly Want a Cracker.mp3',
+	dl6:		'./Sounds/Remember DL-6.mp3',
+	bigcry:		'./Sounds/SQUAAAAAAAWK!.mp3',
+	cry:		'./Sounds/SQUAWK!.mp3',
+};
+
+const IMAGE_NAMES = Object.keys(IMAGE_LOCATIONS);
+const SOUND_NAMES = Object.keys(SOUND_LOCATIONS);
+
+var Sounds = {};
+class SoundManagerClass {
+	constructor() {
+		this._allLoadedListeners = [];
+		this._loadedFlags = {};
+		this._numLeftToLoad = SOUND_NAMES.length;
+		SOUND_NAMES.forEach(name => this._loadedFlags[name] = false);
+	}
+	
+	onDataLoaded(func) {
+		if (this._numLeftToLoad == 0) {
+			func();
+		} else {
+			this._allLoadedListeners.push(func);
+		}
+	}
+	
+	_loadingFinished() {
+		this._allLoadedListeners.forEach(func => func());
+		this._allLoadedListeners = null;
+	}
+	
+	_dataLoaded(name) {
+		if (!this._loadedFlags[name]) {
+			this._loadedFlags[name] = true;
+			this._numLeftToLoad--;
+			if (this._numLeftToLoad == 0) {
+				this._loadingFinished();
+			}
+		}
+	}
+	
+	loadSounds() {
+		Object.keys(SOUND_LOCATIONS).forEach(name => {
+			$(`<audio id="sound_${name}" src="${SOUND_LOCATIONS[name]}">`)
+			.on('loadeddata', () => this._dataLoaded(name))
+			.appendTo('#soundholder');
+			Sounds[name] = document.getElementById(`sound_${name}`);
+		});
+	}
+}
+
+// (function loadSounds() {
+// 	Object.keys(SOUND_LOCATIONS).forEach(name => {
+// 		$(`<audio id="sound_${name}" src="${SOUND_LOCATIONS[name]}">`)
+// 		.on('loadeddata', )
+// 		.appendTo('#soundholder');
+// 		Sounds[name] = document.getElementById(`sound_${name}`);
+// 	});
+// })();
+
+var soundManager = new SoundManagerClass();
+soundManager.loadSounds();
---
+++ Parrot Mate/parrot.js
@@ -0,0 +1,55 @@
+
+
+
+showImage('regular');
+
+// import SEQUENCES from './sequences.js';
+// require('./sequences.js');
+
+var rsp = new RandomSequencePlayer('regular', SEQUENCES, 3000, 6000);
+
+// var lsp = null;
+// var lsp = new LoopingSequencePlayer('regular', SEQUENCES.dl6, 3000);
+// soundManager.onDataLoaded(function () {
+	// lsp = new LoopingSequencePlayer(SEQUENCES.cry, 300000);
+// });
+// window.addEventListener('load', function () {
+// 	lsp = new LoopingSequencePlayer(SEQUENCES.cry, 300000);
+// });
+
+
+
+// function show(imageName) {
+// 	$('#parrot').attr("src", IMAGE_LOCATIONS[imageName]);
+// }
+
+// function actAndSchedule(minTime, maxTime) {
+// 	let imageName = IMAGE_NAMES[randomInt(0, IMAGE_NAMES.length)];
+// 	console.log(`Loading image ${imageName}.`);
+// 	$('#parrot').attr("src", IMAGE_LOCATIONS[imageName]);
+// 	// activeImage.hide();
+// 	// activeImage = Images[imageName];
+// 	// activeImage.show();
+	
+// 	setTimeout(() => actAndSchedule(minTime, maxTime), randomRange(minTime, maxTime));
+// }
+
+
+// var Images = {};
+// var activeImage = null;
+// 
+// function loadImages() {
+// 	Object.keys(IMAGE_LOCATIONS).forEach(name => {
+// 		Images[name] =
+// 		$(`<img id="${name}" class="center-fit" src="${IMAGE_LOCATIONS[name]}">`)
+// 		// .hide()
+// 		.appendTo('#imageholder');
+// 	});
+	
+// 	activeImage = Images.regular;
+// 	activeImage.show();
+// }
+
+
+// loadImages();
+// setTimeout(() => actAndSchedule(1000, 1000), 1000);
