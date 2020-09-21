c64e75dd35c833907bc98a186539dbecf14e6ff2
Tomer Godinger <Todinger@Gmail.com>
2020-09-21 23:39:41 +0300

    Channel Party: Switched to real command names

--- Scripted/Effects/ChannelParty/effect.js
+++ Scripted/Effects/ChannelParty/effect.js
@@ -12,39 +12,45 @@ class ChannelParty extends Effect {
 	load() {
 		this.forwardSimpleTwitchEvent('userJoined');
 		this.forwardSimpleTwitchEvent('userLeft');
-		this.registerCommand('a', [Effect.Filters.isOneOf(['fluxistence', 'yecatsmailbox'])], user => {
-			this.broadcastEvent('show', 'Channel Party');
-		});
-		this.registerCommand('b', [Effect.Filters.isOneOf(['fluxistence', 'yecatsmailbox'])], user => {
-			this.broadcastEvent('hide', 'Channel Party');
-		});
 		this.forwardSimpleCommand('hype', [Effect.Filters.isOneOf(['fluxistence', 'yecatsmailbox'])]);
 		this.forwardSimpleCommand('epyh', [Effect.Filters.isOneOf(['fluxistence', 'yecatsmailbox'])]);
+		
+		this.registerCommand('party', [Effect.Filters.isOneOf(['fluxistence', 'yecatsmailbox'])],
+			(user, level) => {
+				this.broadcastEvent('hype', level);
+			}
+		);
+		this.registerCommand('ytrap', [Effect.Filters.isOneOf(['fluxistence', 'yecatsmailbox'])],
+			(user, level) => {
+				this.broadcastEvent('epyh');
+			}
+		);
+		this.registerCommand('stophype', [Effect.Filters.isOneOf(['fluxistence', 'yecatsmailbox'])],
+			(user, level) => {
+				this.broadcastEvent('endHype');
+			}
+		);
+		this.registerCommand('finish', [Effect.Filters.isOneOf(['fluxistence', 'yecatsmailbox'])],
+			(user, level) => {
+				this.broadcastEvent('finish');
+			}
+		);
+		
+		this.registerCommand('fxvol', [Effect.Filters.isOneOf(['fluxistence', 'yecatsmailbox'])],
+			(user, volume) => {
+				this.broadcastEvent('fxvol', { username: user.name, volume });
+			}
+		);
 		
 		this.registerCommand('h', [Effect.Filters.isOneOf(['fluxistence', 'yecatsmailbox'])],
 			(user, level) => {
 				this.broadcastEvent('hype', level);
 			}
 		);
-		this.registerCommand('e', [Effect.Filters.isOneOf(['fluxistence', 'yecatsmailbox'])],
-			(user, level) => {
-				this.broadcastEvent('epyh');
-			}
-		);
+		
 		this.registerCommand('s', [Effect.Filters.isOneOf(['fluxistence', 'yecatsmailbox'])],
 			(user, level) => {
 				this.broadcastEvent('endHype');
-			}
-		);
-		this.registerCommand('q', [Effect.Filters.isOneOf(['fluxistence', 'yecatsmailbox'])],
-			(user, level) => {
-				this.broadcastEvent('finish');
-			}
-		);
-		
-		this.registerCommand('z', [Effect.Filters.isOneOf(['fluxistence', 'yecatsmailbox'])],
-			(user, volume) => {
-				this.broadcastEvent('fxvol', { username: user.name, volume });
 			}
 		);
 	}
