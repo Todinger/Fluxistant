e3c6484745a52968fd009bf00d86e3a5b78bf492
Tomer Godinger <Todinger@Gmail.com>
2021-01-30 15:53:55 +0200

    Started implementing command messages.

--- command.js
+++ command.js
@@ -1,6 +1,25 @@
 const assert = require('assert').strict;
 const UserFilters = require('./user').Filters;
 
+class Variable {
+	constructor(expr, replacement, replacementIsRegex) {
+		assert(expr instanceof RegExp || typeof expr == 'string');
+		this.expr = expr;
+		
+		assert(typeof replacement == 'string' || typeof replacement == 'function');
+		if (typeof replacement == 'string' && !replacementIsRegex) {
+			// Escape strings that aren't regular expressions
+		}
+		this.replacement = replacement;
+	}
+	
+	consume(message, user) {
+		if (this.expr instanceof RegExp) {
+		
+		}
+	}
+}
+
 class Command {
 	constructor(data) {
 		this.callback = data.callback;
@@ -16,6 +35,7 @@ class Command {
 		this.enabled = data.enabled ?? true;
 		this.cmdname = data.cmdname;
 		this.aliases = data.aliases || [];
+		this.message = data.message;
 		this.cost = data.cost || 0;
 		this.silent = data.silent;
 		this.cooldowns = data.cooldowns;
@@ -35,6 +55,15 @@ class Command {
 			});
 			
 			return userFilters;
+		}
+	}
+	
+	formatMessage(user, modVars) {
+		let result = this.message;
+		if (modVars) {
+			// Object.keys(modVars).forEach(var => {
+			//
+			// });
 		}
 	}
 }
