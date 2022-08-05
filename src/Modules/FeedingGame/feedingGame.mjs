938e0a58d2865d9a59131adfcf2cf6ad49b64802
Tomer Godinger <Todinger@Gmail.com>
2022-08-05 15:05:19 +0300

    Feeding Game: Added food arc animation.
    The food moves correctly right now, but the mouth open foreground image shows up below instead of over the background image.

--- src/Modules/FeedingGame/feedingGame.mjs
+++ src/Modules/FeedingGame/feedingGame.mjs
@@ -1,10 +1,29 @@
 import { ModuleClient } from "/common/moduleClient.mjs";
-import { inRange } from "/common/clientUtils.mjs";
 
 const TRANSPARENT_PIXEL_IMAGE = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
 const NO_IMAGE = TRANSPARENT_PIXEL_IMAGE;
 
-class TreasureHuntClient extends ModuleClient {
+// This takes an asymmetric function f (one for which f(-x) = -f(x) for all x)
+// and creates an easing function g from it with the following properties:
+// 1. g(0) = 0
+// 1. g(0.5) = 0.5
+// 1. g(1) = 1
+function asymmetricFunctionToEasing(f) {
+	const denominator = 2 * f(0.5);
+	return t => f(t - 0.5) / denominator + 0.5;
+}
+
+// For the parameters outerCoefficient = a, exponent = n, innerCoefficient = b,
+// the result of this function is the mathematical function:
+//   f(x) = a * (tan(b * x)) ^ n
+// Note that if n is an odd integer then this function is asymmetric.
+// Also note that if n is fractional or negative then you may get mathematical
+// errors when calculating for some values.
+function transformTangent(outerCoefficient = 1, exponent = 1, innerCoefficient = 1) {
+	return x => outerCoefficient * Math.pow(Math.tan(innerCoefficient * x), exponent);
+}
+
+class FeedingGameClient extends ModuleClient {
 	constructor() {
 		super('Feeding Game');
 		
@@ -22,117 +41,36 @@ class TreasureHuntClient extends ModuleClient {
 		
 		this.foodImage = $('#food');
 		this.setFoodImage(); // Clear the food image
-	}
-	
-	makeCell() {
-		let jContainer = $('<div class="grid-item container"></div>');
-		let jSub = $('<div class="sub"></div>').appendTo(jContainer);
-		let jContents = $(`<img class="center sq" alt="Missing Image" src="${this.stateImages.empty}">`).appendTo(jSub);
 		
-		return {
-			container: jContainer,
-			contents: jContents,
-		};
-	}
-	
-	initGame(height, width) {
-		this.size = {width, height};
-		
-		let evContainer = $('#cells');
-		evContainer.empty();
-		
-		// Set the grid CSS properties to support the number of columns we want
-		let styleString = '';
-		styleString += `grid-template-columns: ${'auto '.repeat(width)}; `;
-		styleString += `grid-template-rows: ${'1fr '.repeat(height)}; `;
-		styleString += `grid-template-areas: ${`'${"main ".repeat(width)}'`.repeat(height)}; `;
-		evContainer[0].style = styleString;
-		// evContainer.css('grid-template-columns', 'auto '.repeat(width));
-		// evContainer.css('grid-template-rows', 'auto ' + '1fr '.repeat(height));
-		// evContainer.css('grid-template-areas', `'${'header '.repeat(width)}' ${`'${'main'.repeat(width)}'`.repeat(height)}`);
-		
-		// evContainer.append($(TITLE));
-		
-		this.cells = [];
-		for (let row = 0; row < height; row++) {
-			let rowCells = [];
-			for (let col = 0; col < width; col++) {
-				let cell = this.makeCell();
-				rowCells.push(cell);
-				evContainer.append(cell.container);
-			}
+		this.foodPathContainer = $('#foodPath');
+		let svg = this.foodPathContainer[0];
+		svg.addEventListener('load', () => {
+			let svgDoc = svg.contentDocument;
+			let foodPath = svgDoc.getElementById('foodPath');
+			let path = foodPath.getAttribute('d');
 			
-			this.cells.push(rowCells);
-		}
-		
-		this.hide();
+			console.log('Creating pathAnimator');
+			this.pathAnimator = new PathAnimator(path, {
+				duration: 1,
+				step: (point, angle) => this.setFoodImagePos(point, angle),
+				easing: asymmetricFunctionToEasing(transformTangent(1, 1, 2)), //t => t * (2 - t),// + 3,
+				onDone: () => this.feedDone(),
+			});
+			console.log('pathAnimator created');
+			
+			this.foodPathOffset = this.foodPathContainer.position();
+			this.foodPathContainer.hide();
+		});
 	}
 	
 	hide() {
-		$('#all').hide();
+		// $('#all').hide();
 	}
 	
 	show() {
 		$('#all').show();
 	}
 	
-	locationValid(row, col) {
-		return inRange(0, row, this.size.height - 1) && inRange(0, col, this.size.width - 1);
-	}
-	
-	setState(row, col, state) {
-		console.assert(this.locationValid(row, col), `Cell indices out of bounds: [${row}, ${col}] outside ${this.size.height} x ${this.size.width} board`);
-		console.assert(state in this.stateImages, `Unknown state: ${state}`);
-		
-		this.cells[row][col].contents.attr('src', this.stateImages[state]);
-	}
-	
-	handleInitGameMessage(data) {
-		if (!data) {
-			this.server.emit('error', 'Empty data object received.');
-			return false;
-		}
-		
-		if (isNaN(data.height) || isNaN(data.width)) {
-			this.server.emit('error', 'Missing board size parameter(s).');
-			return false;
-		}
-		
-		this.initGame(data.height, data.width);
-		this.show();
-		return true;
-	}
-	
-	handleSetStateMessage(data) {
-		if (!data) {
-			this.server.emit('error', 'Empty data object received.');
-			return;
-		}
-		
-		if (isNaN(data.row) || isNaN(data.col)) {
-			this.server.emit('error', 'Not all cell indices present.');
-			return;
-		}
-		
-		let row = data.row - 1;
-		let col = data.col - 1;
-		if (!this.locationValid(row, col)) {
-			this.server.emit('error', `Invalid location: [${data.row}, ${data.col}] is outside ${this.size.height} x ${this.size.width} board`);
-			return;
-		}
-		
-		if (!data.state) {
-			this.server.emit('error', 'Missing state value.');
-			return;
-		}
-		
-		if (!(data.state in this.stateImages)) {
-			this.server.emit('error', `Unknown state value: ${data.state}`);
-			return;
-		}
-		
-		this.setState(row, col, data.state);
-	}
 	
 	open() {
 		this.forms.open.show();
@@ -171,11 +109,19 @@ class TreasureHuntClient extends ModuleClient {
 		this.setFoodImage(data);
 		this.open();
 		this.foodImage.show();
-		setTimeout(() => {
-			this.foodImage.hide();
-			this.close();
-			this.server.emit('feedDone');
-		}, 100);
+		// setTimeout(() => this.feedDone(), 1000);
+		this.pathAnimator.start(0, 100);
+	}
+	
+	setFoodImagePos(point /*, angle */) {
+		let offset = this.foodPathOffset;
+		this.foodImage.css({ top: offset.top + point.y, left: offset.left + point.x });
+	}
+	
+	feedDone() {
+		this.foodImage.hide();
+		this.close();
+		this.server.emit('feedDone');
 	}
 	
 	start() {
@@ -197,5 +143,7 @@ class TreasureHuntClient extends ModuleClient {
 	}
 }
 
-const thc = new TreasureHuntClient();
-thc.start();
+const c = new FeedingGameClient();
+c.start();
+
+window.c = c;
