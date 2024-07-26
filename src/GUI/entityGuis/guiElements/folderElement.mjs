7df5f5f7f083229b93d23d8dea47d93daa4ce5e5
Tomer Godinger <Todinger@Gmail.com>
2024-07-26 20:01:06 +0300

    Config: Performance improvement hack
    This makes all "folder" GUI elements store their contents inside the actual HTML page only when they're open.
    When they close, they save the data aside and remove it from the page.
    This doesn't play that well with unsaved changes when switching modules, but I think it's worth it, for now at least.

--- src/GUI/entityGuis/guiElements/folderElement.mjs
+++ src/GUI/entityGuis/guiElements/folderElement.mjs
@@ -5,7 +5,7 @@ export default class FolderElement extends GuiElement {
 		let accordion = $(`<ul uk-accordion class="uk-margin-small-top uk-box-shadow-large uk-padding-small"></ul>`);
 		let accordionItem = $(`<li></li>`);
 		accordion.append(accordionItem);
-		
+
 		let header = $(`<a class="uk-accordion-title" href="#"></a>`);
 		if (data.header) {
 			header.append(data.header);
@@ -14,19 +14,30 @@ export default class FolderElement extends GuiElement {
 			header.attr('uk-tooltip', data.tooltip);
 		}
 		accordionItem.append(header);
-		
+
 		let contentsContainer = $(`<div class="uk-accordion-content"></div>`);
 		if (data.contents) {
-			contentsContainer.append(data.contents);
+			contentsContainer._conents = data.contents;
 		}
 		accordionItem.append(contentsContainer);
-		
+
 		accordion.guiData = {
 			accordionItem,
 			header,
 			contentsContainer,
 		};
-		
+
+		accordion.on('beforeshow', (event) => {
+			if (event.target === accordionItem[0]) {
+				contentsContainer.append(contentsContainer._conents);
+			}
+		});
+		accordion.on('hidden', (event) => {
+			if (event.target === accordionItem[0]) {
+				contentsContainer._conents = contentsContainer.children(":first");
+			}
+		});
+
 		return accordion;
 	}
 }
